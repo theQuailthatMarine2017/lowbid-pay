9dfa3ed463ad0a992d1412cba1e589cfb79da47f
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-11 15:52:50 +0300

    lslsls

--- routes/routes.js
+++ routes/routes.js
@@ -161,112 +161,111 @@ module.exports = function(app){
 
         connection.connect();
 
-        connection.query('INSERT INTO callback SET ?',[parseString(req.body.Body.stkCallback)], function (err, results) {
-            if (err){
-                console.log(err)
-                const log_ = new log(sys_actions.mpesa.failed,sys_actions.outcome.failed, err, 'mpesa request','mpesa request');
-                            connection.query('INSERT INTO SYS_LOGS SET ?', [log_], function (err) {
-                                if (err){
-                                    console.log("h")
-                                }
-                                })
-            }
-        });
+        // connection.query('INSERT INTO callback SET ?',[parseString(req.body.Body.stkCallback)], function (err, results) {
+        //     if (err){
+        //         console.log(err)
+        //         const log_ = new log(sys_actions.mpesa.failed,sys_actions.outcome.failed, err, 'mpesa request','mpesa request');
+        //                     connection.query('INSERT INTO SYS_LOGS SET ?', [log_], function (err) {
+        //                         if (err){
+        //                             console.log("h")
+        //                         }
+        //                         })
+        //     }
+        // });
         
-        //file written successfully
        
 
-        // console.log("<------ STK RESPONSE ------->")
-        // //PAYMENT HAD ERROR
-        // if(req.body.Body.stkCallback.ResultCode === 1 || req.body.stkCallback.Body.ResultCode === 1032 || req.body.Body.stkCallback.ResultCode === 2001 || req.body.Body.stkCallback.ResultCode === 1037){
-        //     // LOG ERROR 
-        //     console.log('<------FAILED MPESA TRANSACTION-------->');
-        //     const log_ = new log(sys_actions.mpesa.failed,sys_actions.outcome.failed, req.body.Body.stkCallback.ResultDesc, 'callback from mpesa','callback from mpesa');
-        //     connection.query('INSERT INTO SYS_LOGS SET ?', [log_], function (error) {
-        //         if (error){
-        //             res.json({message:"Server Error"});
-        //         }
-        //     });
-        // }else{
-        //     //PAYMENT WAS SUCCESSFUL, ADD MPESA TRANSACTION CODE FOR BID OBJECT FROM STK RESPONSE
-        //     console.log('<------SUCCESSFUL MPESA TRANSACTION-------->');
+        console.log("<------ STK RESPONSE ------->")
+        //PAYMENT HAD ERROR
+        if(req.body.Body.stkCallback.ResultCode === 1 || req.body.stkCallback.Body.ResultCode === 1032 || req.body.Body.stkCallback.ResultCode === 2001 || req.body.Body.stkCallback.ResultCode === 1037){
+            // LOG ERROR 
+            console.log('<------FAILED MPESA TRANSACTION-------->');
+            const log_ = new log(sys_actions.mpesa.failed,sys_actions.outcome.failed, req.body.Body.stkCallback.ResultDesc, 'callback from mpesa','callback from mpesa');
+            connection.query('INSERT INTO SYS_LOGS SET ?', [log_], function (error) {
+                if (error){
+                    res.json({message:"Server Error"});
+                }
+            });
+        }else{
+            //PAYMENT WAS SUCCESSFUL, ADD MPESA TRANSACTION CODE FOR BID OBJECT FROM STK RESPONSE
+            console.log('<------SUCCESSFUL MPESA TRANSACTION-------->');
             
-        //     connection.query('UPDATE BIDS SET PAID = ? WHERE MPESA_CODE =  ?', [1,req.body.Body.stkCallback.CheckoutRequestID], function (error, results) {
-        //                 if (error){
-        //                     console.log('<------BID SAVE ERROR-------->');
-        //                     //LOG ERROR 
-        //                     const log_ = new log(sys_actions.bids.create,sys_actions.outcome.failed, error, 'callback from  mpesa','callback from mpesa');
-        //                     connection.query('INSERT INTO SYS_LOGS SET ?', [log_], function (error) {
-        //                         if (error){
-        //                             console.log(error)
-        //                             res.json({message:"Server Error"});
-        //                         }
-        //                       });
+            connection.query('UPDATE BIDS SET PAID = ? WHERE MPESA_CODE =  ?', [1,req.body.Body.stkCallback.CheckoutRequestID], function (error, results) {
+                        if (error){
+                            console.log('<------BID SAVE ERROR-------->');
+                            //LOG ERROR 
+                            const log_ = new log(sys_actions.bids.create,sys_actions.outcome.failed, error, 'callback from  mpesa','callback from mpesa');
+                            connection.query('INSERT INTO SYS_LOGS SET ?', [log_], function (error) {
+                                if (error){
+                                    console.log(error)
+                                    res.json({message:"Server Error"});
+                                }
+                              });
 
-        //                 }else{
+                        }else{
 
-        //                     //IF STK WAS SUCCESS
-        //                     connection.query('SELECT PRODUCT,BID_AMOUNT FROM BIDS WHERE MPESA_CODE = ?',[req.body.Body.stkCallback.CheckoutRequestID],function (error,bid){
+                            //IF STK WAS SUCCESS
+                            connection.query('SELECT PRODUCT,BID_AMOUNT FROM BIDS WHERE MPESA_CODE = ?',[req.body.Body.stkCallback.CheckoutRequestID],function (error,bid){
 
-        //                         if (error){
-        //                             console.log('<------BID SAVE ERROR-------->');
-        //                             console.log(error)
+                                if (error){
+                                    console.log('<------BID SAVE ERROR-------->');
+                                    console.log(error)
 
-        //                         }else{
+                                }else{
 
-        //                             // SUCCESFUL! UPDATE PRODUCT
-        //                         console.log('<------BID MOUNT UPDATING-------->');
-        //                         connection.query('UPDATE PRODUCTS SET TOTAL_BIDS = TOTAL_BIDS + 1, AMOUNT_RAISED = AMOUNT_RAISED + ? WHERE NAME = ?' , [bid.BID_AMOUNT,bid.PRODUCT], function (error) {
-        //                             if (error){
-        //                                 console.log(error);
-        //                             }else{
-        //                                 // log action
-        //                                 console.log('<------BID AMOUNT UPDATED-------->');
-        //                                 const log_ = new log(sys_actions.products.updated,sys_actions.outcome.success,null, 'callback from mpesa','callback from mpesa');
-        //                                 // Neat!
-        //                                 connection.query('INSERT INTO SYS_LOGS SET ?', [log_], function (error) {
-        //                                     if (error){
-        //                                                 console.log(error);
-        //                                             }else{
-        //                                                 // Neat!
-        //                                                 //Africa Talking SMS to Winning Mobile
-        //                                                 const options = {
-        //                                                     to: [bid.MOBILE_NO],
-        //                                                     message: 'Your Bid Has Been Successfully Placed! You Can Place As Many Bids As You Want! Thank You For Choosing Lowbids!'
-        //                                                 }
+                                    // SUCCESFUL! UPDATE PRODUCT
+                                console.log('<------BID MOUNT UPDATING-------->');
+                                connection.query('UPDATE PRODUCTS SET TOTAL_BIDS = TOTAL_BIDS + 1, AMOUNT_RAISED = AMOUNT_RAISED + ? WHERE NAME = ?' , [bid.BID_AMOUNT,bid.PRODUCT], function (error) {
+                                    if (error){
+                                        console.log(error);
+                                    }else{
+                                        // log action
+                                        console.log('<------BID AMOUNT UPDATED-------->');
+                                        const log_ = new log(sys_actions.products.updated,sys_actions.outcome.success,null, 'callback from mpesa','callback from mpesa');
+                                        // Neat!
+                                        connection.query('INSERT INTO SYS_LOGS SET ?', [log_], function (error) {
+                                            if (error){
+                                                        console.log(error);
+                                                    }else{
+                                                        // Neat!
+                                                        //Africa Talking SMS to Winning Mobile
+                                                        const options = {
+                                                            to: [bid.MOBILE_NO],
+                                                            message: 'Your Bid Has Been Successfully Placed! You Can Place As Many Bids As You Want! Thank You For Choosing Lowbids!'
+                                                        }
 
-        //                                                 sms.send(options)
-        //                                                     .then( response => {
+                                                        sms.send(options)
+                                                            .then( response => {
 
-        //                                                         console.log(response.SMSMessageData["Recipients"])
-        //                                                         if(response["Recipients"] != []){
-        //                                                             res.json({message:"Bid Placed"});
+                                                                console.log(response.SMSMessageData["Recipients"])
+                                                                if(response["Recipients"] != []){
+                                                                    res.json({message:"Bid Placed"});
 
-        //                                                         }else{
-        //                                                             res.json({message:response.Message});
-        //                                                         }
-        //                                                     })
-        //                                                     .catch( error => {
+                                                                }else{
+                                                                    res.json({message:response.Message});
+                                                                }
+                                                            })
+                                                            .catch( error => {
 
-        //                                                         console.log(error)
-        //                                                         res.json({message:error});
-        //                                                     });
+                                                                console.log(error)
+                                                                res.json({message:error});
+                                                            });
                                                         
-        //                                             }
-        //                                         });
-        //                                     }
-        //                                 });
+                                                    }
+                                                });
+                                            }
+                                        });
 
-        //                         }
+                                }
 
                         
-        //                     })
+                            })
                             
                             
-        //                 }
-        //               });
+                        }
+                      });
 
-        // }
+        }
 
     });
 }
