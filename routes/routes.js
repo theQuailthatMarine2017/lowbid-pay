94d0346f28d26f98584013c221634d8d50916d7c
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-11 11:23:39 +0300

    lslsls

--- routes/routes.js
+++ routes/routes.js
@@ -136,14 +136,15 @@ module.exports = function(app){
             }}).then( res => {
                 console.log('<-------MPESA TRANSACTION SENT SUCCESSFULLY--------->');
                 let bid_ = bid.bids(bidobject.name,bidobject.bid_placed,bidobject.lowest_bid,bidobject.mobile,bidobject.category,res.data.MerchantRequestID);
-                connection.query('INSERT INTO BIDS SET ?', [bid_], function (error, results) {
-                    if (error){
-                        res.json({message:error,bid:bid_ });
-                    }else{
-                        response.json({message:"Payment Request Receieved. Processing"})
-                    }
+                res.json({message:bid_ });
+                // connection.query('INSERT INTO BIDS SET ?', [bid_], function (error, results) {
+                //     if (error){
+                //         res.json({message:error,bid:bid_ });
+                //     }else{
+                //         response.json({message:"Payment Request Receieved. Processing",bid:bid_})
+                //     }
                     
-                });
+                // });
                 
             }).catch(error => {
                 response.json({message:error});
