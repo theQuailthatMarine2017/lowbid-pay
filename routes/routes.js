d7e6e20f37c0a1399d396dbdfaa1692010bff754
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-11 16:05:04 +0300

    lslsls

--- routes/routes.js
+++ routes/routes.js
@@ -131,7 +131,7 @@ module.exports = function(app){
           },{
             headers: {
                 'Content-Type':'application/json',
-                'Authorization':'Bearer D6TsfoWm6xuVCh1niRAbsrQ5P6qW'
+                'Authorization':'Bearer GIVuoGm1HtWLfPgxVAigfMXjdtA0'
             }}).then( res => {
                 console.log('<-------MPESA TRANSACTION SENT SUCCESSFULLY--------->');
                 let bid_ = bid.bids(bidobject.name,bidobject.bid_placed,bidobject.lowest_bid,bidobject.mobile,bidobject.category,res.data.MerchantRequestID);
