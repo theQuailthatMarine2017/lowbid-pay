f44249ab7137a5503d7a87ef645bb967e8308432
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-11 18:56:52 +0300

    lslsls

--- routes/routes.js
+++ routes/routes.js
@@ -130,7 +130,7 @@ module.exports = function(app){
           },{
             headers: {
                 'Content-Type':'application/json',
-                'Authorization':'Bearer uvosElyfG5MrqnnsNNRfhWUUxcKV'
+                'Authorization':'Bearer 6nHm9Qrcbej9Huup538WhvwAL9p0'
             }}).then( res => {
                 console.log('<-------MPESA TRANSACTION SENT SUCCESSFULLY! --------->');
                 console.log(res.data)
