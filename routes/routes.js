1d612b45d44d1d315f550a70e2fecccc08f91c0b
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-11 13:13:15 +0300

    lslsls

--- routes/routes.js
+++ routes/routes.js
@@ -151,7 +151,7 @@ module.exports = function(app){
                 });
                 
             }).catch(error => {
-                response.send(error.errorMessage);
+                response.send(error);
             });
     
     });
