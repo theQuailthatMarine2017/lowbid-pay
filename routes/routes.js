6a49317254d50ead6f0cefe86e6449d517f15e8b
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-11 12:32:23 +0300

    lslsls

--- routes/routes.js
+++ routes/routes.js
@@ -120,7 +120,7 @@ module.exports = function(app){
             "Password": base64string,
             "Timestamp": timestamp,
             "TransactionType": "CustomerPayBillOnline",
-            "Amount": 5,
+            "Amount": 4,
             "PartyA": parseInt(bidobject.mobile),
             "PartyB": 4084101,
             "PhoneNumber": parseInt(bidobject.mobile),
