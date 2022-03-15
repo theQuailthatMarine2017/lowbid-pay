c7e1b34eec0801ccdb1fa68868ad0212113043a3
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-15 14:32:57 +0300

    testing

--- token.js
+++ token.js
@@ -8,8 +8,8 @@ var consumerSecret = process.env.SECRET //Enter consumerSecret issues by daraja
 
 //Generates token every 50Min From CronJob
 auth.generate(environment,consumerKey,consumerSecret,function(token){
-
     console.log("PreviousToken: " + process.env.AUTH_TOKEN)
     console.log("Generated Token: " + token.code)
     process.env.AUTH_TOKEN = token.code
+    console.log("Updated Token: " + process.env.AUTH_TOKEN)
 });
