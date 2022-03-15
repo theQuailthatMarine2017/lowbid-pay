74f4aee418040d86699f35c652e8342fb391bd35
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-15 13:50:59 +0300

    testing

--- token.js
+++ token.js
@@ -5,7 +5,10 @@ var environment = process.env.ENVIRONMENT //Can be either 'production' or 'sandb
 var consumerKey = process.env.KEY //Enter consumerKey issued by Daraja
 var consumerSecret = process.env.SECRET //Enter consumerSecret issues by daraja
 
+
 //Generates token every 50Min From CronJob
 auth.generate(environment,consumerKey,consumerSecret,function(token){
+
+    console.log(token)
     process.env.AUTH_TOKEN = token.code
 });
